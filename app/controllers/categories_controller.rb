80d3463a42bf33de153ef2f8763d3bc803f22b51
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-10 15:37:45 +0000

    Added the new and create to the category also made it so you can select products but it may not be the best way to do as when you have a lot of products to list would be giant

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,8 +10,32 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
+    @products = Product.all
   end
 
   def create
+    @category = Category.new(category_params)
+    product_params[:products].each do |product|
+      if product.length > 0
+        @product = Product.find(product.to_i)
+        @category.products << @product
+      end
+    end
+    if @category.save
+      redirect_to category_path(@category)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params[:category].require(:product_categories).permit({products:[]})
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
