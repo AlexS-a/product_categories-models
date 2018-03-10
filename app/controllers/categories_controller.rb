1a3cfef9c8c68f4be0597401031a39dbec207344
Alexander Sips-Avello <alex.s.a1994@gmail.com>
2018-03-10 15:07:28 +0000

    Added the category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,6 +5,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
   end
 
   def new
